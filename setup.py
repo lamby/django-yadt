91affa8b785e0b5261f69448c1c08de429460bb9
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-02 10:35:36 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='django-yadt',
+    packages=(
+        'django_yadt',
+        'django_yadt.management',
+        'django_yadt.management.commands',
+    ),
+)
