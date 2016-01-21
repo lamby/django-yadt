e769a6d7a8e1f07a167e9372fc588c26ac6a8142
Chris Lamb <chris@chris-lamb.co.uk>
2016-01-21 14:45:19 +0100

    Releasing version 2.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-yadt',
 
     url="https://chris-lamb.co.uk/projects/django-yadt",
-    version='2.0.2',
+    version='2.0.3',
     description="Yet Another Django Thumbnailer",
 
     author="Chris Lamb",
