65bca365e932191e702feb064595cc3aa2ea7ed1
Chris Lamb <chris@chris-lamb.co.uk>
2016-02-07 19:14:37 +0100

    Tidy setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 setup(
     name='django-yadt',
 
-    url="https://chris-lamb.co.uk/projects/django-yadt",
+    url='https://chris-lamb.co.uk/projects/django-yadt',
     version='2.0.3',
     description="Yet Another Django Thumbnailer",
 
     author="Chris Lamb",
-    author_email="chris@chris-lamb.co.uk",
-    license="BSD",
+    author_email='chris@chris-lamb.co.uk',
+    license='BSD',
 
     packages=find_packages(),
 )
